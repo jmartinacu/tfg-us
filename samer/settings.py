ce3156980b15b35e2cf29e16d20bc013708d8a45
jmartinacu <jmartinacu2002@gmail.com>
2024-11-12 20:58:03 +0100

    conf: Created django postgresql connection

--- samer/settings.py
+++ samer/settings.py
@@ -62,14 +62,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "samer.wsgi.application"
 
-
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.getenv("DB_NAME", "samer"),
+        "USER": os.getenv("DB_USER", "postgres"),
+        "PASSWORD": os.getenv("DB_PASS", ""),
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
