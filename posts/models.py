36aa5c482e96e9034812026d6097352de3bb35e0
jmartinacu <jmartinacu2002@gmail.com>
2024-11-13 19:26:49 +0100

    feat: Created post and comment models

--- posts/models.py
+++ posts/models.py
@@ -1,1 +1,34 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Post(models.Model):
+    class PostTypes(models.TextChoices):
+        IMAGE = "IM", _("Image")
+        VIDEO = "VD", _("Video")
+
+    name = models.CharField(max_length=30)
+    object_names = ArrayField(
+        models.CharField(max_length=30),
+        blank=True,
+        null=True,
+    )
+    urls = ArrayField(models.URLField(), blank=True)
+    thumbnail_url = models.URLField(blank=True, null=True)
+    likes = ArrayField(models.CharField(max_length=100), blank=True)
+    description = models.TextField(blank=True, null=True)
+    post_type = models.CharField(
+        max_length=2,
+        choices=PostTypes,
+        default=PostTypes.IMAGE,
+    )
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments",
+    )
