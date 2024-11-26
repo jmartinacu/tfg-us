a75d6c19a8c46db0fd7c07eb07e41dbbb5beda3a
jmartinacu <jmartinacu2002@gmail.com>
2024-11-26 20:51:14 +0100

    feat: Created profile model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
-# Create your models here.
+
+class ProfileInformation(models.Model):
+    app_name = models.CharField(max_length=255)
+    app_real_name = models.CharField(max_length=255)
+    descriptions = ArrayField(models.CharField(max_length=100), blank=True)
+    url = models.URLField(blank=True, null=True)
+    image_url = models.URLField()
