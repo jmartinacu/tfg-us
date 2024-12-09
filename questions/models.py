9ca7e89d995a5007fc869f853f3c59cdb3b5b6dd
jmartinacu <jmartinacu2002@gmail.com>
2024-12-09 21:07:11 +0100

    feat: Created models for question app

--- questions/models.py
+++ questions/models.py
@@ -1,3 +1,36 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    resolve = models.BooleanField(default=False)
+    archive = models.BooleanField(default=False)
+    answer = models.OneToOneField(
+        "Answer", on_delete=models.SET_NULL, null=True, blank=True
+    )
+    likes = models.ManyToManyField(
+        User,
+        related_name="liked_questions",
+        blank=True,
+    )
+    tags = models.JSONField(default=list)
+    views = models.ManyToManyField(
+        User,
+        related_name="viewed_questions",
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class Answer(models.Model):
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.OneToOneField(Question, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Answer to {self.question.title}"
