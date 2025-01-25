9e81417bae91ecb2f2b64f1f7d861c9c3625ed95
jmartinacu <jmartinacu2002@gmail.com>
2025-01-25 20:23:37 +0100

    feat: Added tags to root template

--- root/views.py
+++ root/views.py
@@ -1,28 +1,24 @@
+from django.db import connection
+from django.db.models import Count
 from django.shortcuts import render
 
 from posts.models import Post
 
 
 def root(request):
-    posts = Post.objects.all()
-    # posts = [
-    #     {
-    #         "id": str(post["_id"]),
-    #         "name": post["name"],
-    #         "comments": mongo_comment.count(query={"post": str(post["_id"])}),
-    #         "likes": post["likes"],
-    #         "type": "imagen" if post["type"] == "image" else "video",
-    #         "tags": ", ".join(
-    #             [
-    #                 tag["name"]
-    #                 for tag in mongo_tag.find(
-    #                     query={"posts": str(post["_id"])},
-    #                 )
-    #             ]
-    #         ),
-    #     }
-    #     for post in posts_db
-    # ]
+    posts = Post.objects.all().annotate(
+        comments=Count("comments"),
+    )
+    is_postgresql = connection.vendor == "postgresql"
+    if is_postgresql:
+        from django.contrib.postgres.aggregates import StringAgg
+
+        posts.annotate(
+            tags=StringAgg("tags__name", delimiter=","),
+        )
+    else:
+        for post in posts:
+            post.tags = ",".join(tag.name for tag in post.tags)
     return render(
         request,
         "root/posts/posts.html",
