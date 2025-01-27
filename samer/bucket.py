435f95b3149be6d64ca3dc01f836bae1ae32fe28
jmartinacu <jmartinacu2002@gmail.com>
2025-01-28 00:02:48 +0100

    feat: Created upload_thumbnail to bucket method

--- samer/bucket.py
+++ samer/bucket.py
@@ -1,3 +1,7 @@
+import os
+import subprocess
+from io import BytesIO
+
 import boto3
 from django.conf import settings
 
@@ -14,3 +18,32 @@ def upload_file(file: bytes, object_name: str):
 def delete_file(object_name: str):
     bucket = settings.AWS_BUCKET_NAME
     s3.delete_object(Bucket=bucket, Key=object_name)
+
+
+def upload_thumbnail(frame: int, video_url: str):
+    command = [
+        "ffmpeg",
+        "-i",
+        video_url,
+        "-vf",
+        f"select='eq(n,{frame})'",
+        "-q:v",
+        "3",
+        "-f",
+        "image2pipe",
+        "-vcodec",
+        "mjpeg",
+        "pipe:1",
+    ]
+    process = subprocess.run(
+        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=False
+    )
+    if process.returncode != 0:
+        raise Exception(f"ffmpeg error: {process.stderr.decode('utf-8')}")
+    video_name = video_url.split("/")[-1]
+    thumb_name, _thumb_ext = os.path.splitext(video_name)
+    thumb_io = BytesIO(process.stdout)
+    thumbnail_url = upload_file(
+        file=thumb_io, object_name=f"videos/thumbnails/{thumb_name}.jpg"
+    )
+    return thumbnail_url
