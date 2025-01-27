a55e73ea220a022243f5d60d2b2d53c2ad337227
jmartinacu <jmartinacu2002@gmail.com>
2025-01-28 00:03:11 +0100

    feat: Added file upload to bucket post creation

--- posts/models.py
+++ posts/models.py
@@ -1,14 +1,32 @@
+import tempfile
+from io import BytesIO
+from os import path
+
+import imageio
 import magic
 import requests
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from PIL import Image
+
+from samer.bucket import delete_file, upload_file, upload_thumbnail
+
+
+class FilesTypes(models.TextChoices):
+    IMAGE = "IM", _("Image")
+    VIDEO = "VD", _("Video")
 
 
 class Source(models.Model):
     url = models.URLField()
     thumbnail_url = models.URLField(blank=True, null=True)
     name = models.CharField(max_length=255)
+    type = models.CharField(
+        max_length=2,
+        choices=FilesTypes.choices,
+    )
+    extension = models.CharField(max_length=255)
 
     def get_mime_type(self):
         result = None
@@ -19,28 +37,119 @@ class Source(models.Model):
         response.close()
         return result
 
+    def get_file_bytes(self, thumbnail=False):
+        url = self.thumbnail_url if thumbnail else self.url
+        response = requests.get(url, stream=True)
+        response.raise_for_status()
+        return response.content
+
+    def is_image(self):
+        try:
+            Image.open(BytesIO(self.get_file_bytes()))
+            return True
+        except IOError:
+            return False
+
+    def is_video(self):
+        with tempfile.NamedTemporaryFile(
+            suffix=".mp4",
+            delete=True,
+        ) as temp_file:
+            temp_file.write(self.get_file_bytes())
+            temp_file.flush()
+            reader = imageio.get_reader(temp_file.name)
+            try:
+                _ = reader.get_next_data()
+                is_video_file = True
+            except RuntimeError:
+                is_video_file = False
+            reader.close()
+            return is_video_file
+
+    def __init__(self, *args, **kwargs):
+        file = kwargs.get("file", None)
+        file_bytes = kwargs.get("file_bytes", None)
+        if file is None:
+            raise ValueError("NoFile")
+        full_file_name = str(file.name)
+        file_name, ext = path.splitext(full_file_name)
+        file_type = (
+            FilesTypes.IMAGE
+            if self.is_image(
+                file_bytes,
+            )
+            else FilesTypes.VIDEO
+        )
+        object_name = (
+            f"imagenes/{full_file_name}"
+            if file_type == FilesTypes.IMAGE
+            else f"videos/{full_file_name}"
+        )
+        uploaded_file_url = upload_file(
+            file,
+            object_name=object_name,
+        )
+        thumbnail_url = None
+        if file_type == FilesTypes.VIDEO:
+            thumbnail_url = upload_thumbnail(0, uploaded_file_url)
+        kwargs["url"] = uploaded_file_url
+        kwargs["name"] = file_name
+        kwargs["type"] = file_type
+        kwargs["thumbnail_url"] = thumbnail_url
+        kwargs["extension"] = ext
+        super().__init__(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        delete_file(self.name)
+        super().delete(*args, **kwargs)
+
     def __str__(self):
         return self.name
 
 
 class Post(models.Model):
-    class PostTypes(models.TextChoices):
-        IMAGE = "IM", _("Image")
-        VIDEO = "VD", _("Video")
-
     name = models.CharField(max_length=255)
     likes = models.ManyToManyField(User, related_name="likes", blank=True)
     description = models.TextField(blank=True, null=True)
     post_type = models.CharField(
         max_length=2,
-        choices=PostTypes,
-        default=PostTypes.IMAGE,
+        choices=FilesTypes.choices,
+        default=FilesTypes.IMAGE,
     )
     sources = models.ForeignKey(
         Source,
         on_delete=models.CASCADE,
         related_name="post",
     )
+
+    def create(self, *args, **kwargs):
+        files = kwargs.get("files", None)
+        name = kwargs.get("name", None)
+        if files is None:
+            raise ValueError("NoFiles")
+        if name is None:
+            full_file_name = str(files[0].name)
+            file_name, _ext = path.splitext(full_file_name)
+            kwargs["name"] = file_name
+        check_source_name = Source.objects.filter(name=name)
+        if check_source_name.exists():
+            raise ValueError("DuplicateName")
+        sources: list[Source] = []
+        for file in files:
+            post_bytes = file.read()
+            file.seek(0)
+            src = Source(file=file, file_bytes=post_bytes)
+            sources.append(src)
+        if len(
+            sources,
+        ) > 1 and not all(s.type != FilesTypes.IMAGE for s in sources):
+            for src in sources:
+                src.delete()
+            raise ValueError("IncorrectFiles")
+        sources = Source.objects.bulk_create(sources)
+        kwargs["sources"] = sources
+        kwargs["post_type"] = sources[0].type
+        super().create(*args, **kwargs)
 
     def __str__(self):
         return self.name
