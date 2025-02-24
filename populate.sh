753ba76231de08be525f26beac3adab0531dc18b
jmartinacu <jmartinacu2002@gmail.com>
2025-02-24 23:58:01 +0100

    feat: Created populate script

---
+++ populate.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+if [ $# -ne 1 ]; then
+    echo "Data loading script requires a single argument: the name of the settings file to use 'local' or 'prod'"
+    exit 1
+fi
+
+FIXTURES=(
+    "users.json"
+    "posts.json"
+    "sources.json"
+    "comments.json"
+    "tags.json"
+    "profile.json"
+    "answers.json"
+    "questions.json"
+)
+
+echo "Beginning to load data"
+
+for fixture in "${FIXTURES[@]}"; do
+    echo "Loading data from $fixture"
+    python manage.py loaddata $fixture --settings="samer.settings.$1"
+done
+
+echo "Data loaded successfully"
