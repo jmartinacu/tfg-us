91aaf949e5bba84b74cd65285d2611e17f25fbc2
jmartinacu <jmartinacu2002@gmail.com>
2025-02-25 21:28:59 +0100

    feat: Added apps urls to main app

--- samer/urls.py
+++ samer/urls.py
@@ -3,5 +3,9 @@ from django.urls import include, path
 
 urlpatterns = [
     path("", include("home.urls", namespace="home")),
+    path("users/", include("users.urls", namespace="users")),
+    path("posts/", include("posts.urls", namespace="posts")),
+    path("questions/", include("questions.urls", namespace="questions")),
+    path("root/", include("root.urls", namespace="root")),
     path("admin/", admin.site.urls),
 ]
